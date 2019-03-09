3809363ba2570eca5a73421675f5b421447f9240
Teodora Ioana <teo.tem06@yahoo.com>
2019-03-09 22:36:28 +0200

    second commit

--- app/src/main/java/com/example/teodora/tictac/MainActivity.java
+++ app/src/main/java/com/example/teodora/tictac/MainActivity.java
@@ -122,7 +122,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void updatePointsText(){
         textViewPlayer1.setText("Player 1:"+player1Points);
-        textViewPlayer2.setText("Player 1:"+player2Points);
+        textViewPlayer2.setText("Player 2:"+player2Points);
     }
 
     private void resetBoard(){
@@ -155,6 +155,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         outState.putInt("roundCount", roundCount);
         outState.putInt("player1Points",player1Points);
         outState.putInt("player2Points",player2Points);
+        outState.putBoolean("player1Turn",player1Turn);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        roundCount = savedInstanceState.getInt("roundCount");
+        player1Points = savedInstanceState.getInt("player1Points");
+        player2Points = savedInstanceState.getInt("player2Points");
+        player1Turn = savedInstanceState.getBoolean("player1Turn");
 
     }
+
+
 }
